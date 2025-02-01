246783275353650827ab76e62163f1e8ff86d98e
techtaka-tia <tia@techtaka.com>
2025-02-01 13:33:09 +0900

    git push 시에 동작하던 action 제거 및 유저 추가 생성

--- src/main/kotlin/com/review/study/issue/auto/repository/user/UserRepository.kt
+++ src/main/kotlin/com/review/study/issue/auto/repository/user/UserRepository.kt
@@ -7,11 +7,11 @@ class UserRepository {
     fun findAll(): List<User> {
         return listOf(
             User(username = "조현준", loginId = "Tianea2160"),
-//            User(username = "김찬웅", loginId = "Dove-kim"),
-//            User(username = "김준우", loginId = "Junuu"),
-//            User(username = "김도엽", loginId = "BrianDYKim"),
-//            User(username = "우경준", loginId = "Jay-WKJun"),
-//            User(username = "곽다희", loginId = "daadaadaah"),
+            User(username = "김찬웅", loginId = "Dove-kim"),
+            User(username = "김준우", loginId = "Junuu"),
+            User(username = "김도엽", loginId = "BrianDYKim"),
+            User(username = "우경준", loginId = "Jay-WKJun"),
+            User(username = "곽다희", loginId = "daadaadaah"),
         )
     }
 }
